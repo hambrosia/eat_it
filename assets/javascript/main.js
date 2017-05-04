f03ab1ccdd30d076666d871fa2fb37cd92c5bb2d
Matt Hambro <matt@Matthews-MacBook-Pro.local>
2017-05-04 18:18:47 -0400

    added bar visuals for daily recommendations

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -62,24 +62,65 @@ $(function() {
         //console.log(searchData);
         renderHTML(reportData);
 
-    },
-    error       : function() {
+      },
+      error       : function() {
+
+      }
+    });
+  }
+
+  var renderHTML = function(reportData){
+
+    //INITIAL TITLE OF FOOD CARD
+    var $foodcard = $("<div class='foodcard col-md-12'></div>");
+    $($foodcard).append("<h1>" + reportData.report.food.name + "</h1>");
+    $($foodcard).append("<h2>" + "100g contains:</h2>");
+
+    //LOOP THROUGH NUTRIENTS AND MAKE BARS
+    for(i=0; i< reportData.report.food.nutrients.length; i++){
+
+      // IF CALORIES MAKE A CALORIES BAR, MAX 2000
+      if (reportData.report.food.nutrients[i].name === "Energy"){
+
+        var cals = reportData.report.food.nutrients[i].value;
+
+        if(cals >= 2000){
+          cals = 2000;
+        }
+        var percent = (cals / 20);
+        percent = Math.floor(percent);
+
+
+        $($foodcard).append("<h3>" + cals + " calories or " + percent + "% of daily recommendation.</h3>");
+        var $progress = $("<div class='progress'></div>");
+        $($progress).append("<div class='progress-bar' role='progressbar' aria-valuenow='" + percent + "'aria-valuemin='0' aria-valuemax='100' style='width:"+ percent +"%''></div>");
+        ($foodcard).append($progress);
+      }
+
+      //IF PROTEIN MAKE A PROTEIN BAR, MAX 112
+      if (reportData.report.food.nutrients[i].name === "Protein"){
+
+        var protein = reportData.report.food.nutrients[i].value;
+
+        if(protein >= 112){
+          protein = 112;
+        }
+        var percent = (protein / 112) *100;
+        percent = Math.floor(percent);
+
+
+        $($foodcard).append("<h3>" + protein + " grams of protein or " + percent + "% of daily recommendation.</h3>");
+        var $progress = $("<div class='progress'></div>");
+        $($progress).append("<div class='progress-bar' role='progressbar' aria-valuenow='" + percent + "'aria-valuemin='0' aria-valuemax='100' style='width:"+ percent +"%''></div>");
+        ($foodcard).append($progress);
+      }
+
+      /* $($foodcard).append("<p>" + reportData.report.food.nutrients[i].name +": " + reportData.report.food.nutrients[i].value + reportData.report.food.nutrients[i].unit + "</p>"); */
 
     }
-  });
-}
-
-var renderHTML = function(reportData){
-
-  var $foodcard = $("<div class='foodcard col-xs-12'></div>");
-  $($foodcard).append("<p>" + "<b>" + reportData.report.food.name + "</b>" + "</p>");
-  for(i=0; i< reportData.report.food.nutrients.length; i++){
-    $($foodcard).append("<p>" + reportData.report.food.nutrients[i].name +": " + reportData.report.food.nutrients[i].value + reportData.report.food.nutrients[i].unit + "</p>");
     $("#results-panel").append($foodcard);
 
   }
-
-}
 
 
 });
